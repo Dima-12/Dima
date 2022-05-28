561f7fabf1d11aa0762f5e4150e0feaa8baaea12
Dima-12 <79586947+Dima-12@users.noreply.github.com>
2022-05-28 15:53:11 +0500

    Update and rename библиотека/storage/lib_file.py to библиотека/Хранение/lib_file.py

---
+++ библиотека/Хранение/lib_file.py
@@ -0,0 +1,12 @@
+from interfaces.manager_library import Library
+import json
+
+class Libfile(Library):
+    def __init__(self, config = {}):
+        self.storage = list()
+        self.file = config.get('file', '/storage/lib.json')
+        if exists(self.file):
+            f = open(self.file)
+            data = f.read()
+            print(data)
+            f.close()
